9b818f10b53a42362e90ea5190e9608af9ae3cb0
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-04-06 22:35:55 +0900

    Incredible

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,26 +5,40 @@ let add_list = $("#chat-group-users");
   function appendUser(user){
     let html = `<div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${user.name}</p>
-        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}", data-user-name="${user.name}">追加</a>
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-name="${user.name}">追加</a>
+        <a data-user-id="${user.id}"></a>
         </div>
     `
     search_list.append(html);
-  }
+  }//インクリメンタルサーチ用の関数
+
 
 
   $(document).on("click", ".chat-group-user__btn--add",function(){
-    let html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-  <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
+    let html = `<div class='chat-group-user clearfix js-chat-member' >
+  <input name='group[user_ids][]' type='hidden' value="${$(this).next().data('user-id')}">
   <p class='chat-group-user__name padding-left'>${$(this).data('user-name')}</p>
-  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-name=${$(this).data('user-name')}>削除</a>
+    <a data-user-id="${$(this).next().data('user-id')}"></a>
 </div>
     `
+
+  $(this).parent().remove();
+  add_list.append(html);
+  });//グループ登録画面へ移動
+
+
+  $(document).on("click", ".chat-group-user__btn--remove",
+    function(){
+          let html = `<div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${$(this).data('user-name')}</p>
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-name=${$(this).data('user-name')}>追加</a><a data-user-id="${$(this).data('user-id')}"></a>
+          <a data-user-id="${$(this).next().data('user-id')}"></a>
+        </div>    `
     $(this).parent().remove();
-    add_list.append(html);
-    // $(this).addClass("chat-group-user__btn--remove");
-    // $(this).html("削除");
-    console.log(this);
-  });
+    search_list.append(html);
+    });//選択場所に戻す
+
 
   function appendErrMsgToHTML(){
     let html = '<div class="error">一致するユーザーが見つかりません</div>'

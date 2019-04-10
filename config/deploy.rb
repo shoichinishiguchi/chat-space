289d33bf28d772c2dcd983d0ae4cb80db2edb326
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-04-11 00:33:32 +0900

    Ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,8 @@
 lock '~> 3.11.0'
 
 set :application, 'chat-space'
-set :repo_url, "git@github.com:shoichinishiguchi/chat-space.git"
+set :repo_url, 'git@github.com:shoichinishiguchi/chat-space.git'
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
